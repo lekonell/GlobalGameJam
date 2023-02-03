7d35a1a50db73db19af7f567782e2befe7f1d019
lekonell <secure@sogang.ac.kr>
2023-02-03 22:20:22 +0900

    02032220

--- Assets/02_Scripts/CameraManager.cs
+++ Assets/02_Scripts/CameraManager.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using GameManager;
+using DG.Tweening.Core.Easing;
 
-public class CameraManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class CameraManager : MonoBehaviour {
+	private Camera cameraObject;
+	private Vector3 cameraOffset = new Vector3(0.0f, 0.0f, -10.0f);
+	private float smoothCoefficient = 0.15f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private void Start() {
+		GameManager.GameManager.Init();
+	}
+
+	private void Awake() {
+		cameraObject = GetComponent<Camera>();
+	}
+
+	public void MoveCamera(Vector3 position) {
+		transform.position = position;
+	}
+
+	public void MoveCameraLerp(Vector3 position) {
+		MoveCameraLerp(position, smoothCoefficient);
+	}
+
+	public void MoveCameraLerp(Vector3 position, float smoothCoefficient) {
+		transform.position = Vector3.Lerp(transform.position, position + cameraOffset, smoothCoefficient);
+	}
 }
---
+++ Assets/02_Scripts/GameManager.cs
@@ -0,0 +1,150 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GameManager {
+	/*
+	 * GameManager는 CamaraManager로부터 Init()된다.
+	 * Scripting Execution Order에 따라 우선순위는 EventSystem > CameraManager > [Other Scripts]
+	 */
+	public static class GameManager {
+		public static bool isInited = false;
+		public static bool isPaused = false;
+		public static bool isPausingOverlay = false;
+
+		public static void Init() {
+			if (isInited)
+				return;
+
+			isInited = true;
+
+			ItemManager.Register("Player", new Item("Player", GameObject.Find("Player")));
+
+			//GameObject Objects = GameObject.Find("Objects");
+			//for (int i = 0; i < Objects.transform.childCount; i++) {
+			//	GameObject ObjectsToBeRegistered = Objects.transform.GetChild(i).gameObject;
+			//	ItemManager.Register(ObjectsToBeRegistered.name, ObjectsToBeRegistered);
+			//	// ObjectsToBeRegistered.SetActive(false);
+			//}
+
+			//Objects.SetActive(false);
+		}
+
+		public static void Pause() {
+			if (isPaused || isPausingOverlay)
+				return;
+
+			isPaused = true;
+		}
+
+		public static void Resume() {
+			if (!isPaused || !isPausingOverlay)
+				return;
+
+			isPaused = false;
+		}
+
+		public static void ShowPauseOverlay() {
+			if (isPaused || isPausingOverlay)
+				return;
+
+			isPausingOverlay = true;
+			ItemManager.Find("PauseOverlay").SetActive(true);
+		}
+
+		public static void HidePauseOverlay() {
+			if (!isPaused || !isPausingOverlay)
+				return;
+
+			isPausingOverlay = false;
+			ItemManager.Find("PauseOverlay").SetActive(false);
+		}
+
+		public static bool GetPausedState() {
+			return isPaused;
+		}
+
+		public static bool GetPausingOverlayState() {
+			return isPausingOverlay;
+		}
+	}
+
+	public class Item {
+		private string itemName { get; }
+		private GameObject itemObject { get; }
+
+		public Item(string _itemName, GameObject _itemObject) {
+			itemName = _itemName;
+			itemObject = _itemObject;
+		}
+
+		public string GetItemName() {
+			return itemName;
+		}
+
+		public GameObject GetItemObject() {
+			return itemObject;
+		}
+	}
+
+	public static class ItemManager {
+		public static Dictionary<string, Item> itemMap;
+		public static bool isInited = false;
+
+		public static void Init() {
+			if (isInited)
+				return;
+
+			isInited = true;
+			itemMap = new Dictionary<string, Item>();
+		}
+
+		public static void EnsureInited() {
+			if (!isInited)
+				Init();
+		}
+
+		public static bool Register(string itemName, Item itemObject, bool isOverwritable = false) {
+			EnsureInited();
+
+			if (!isOverwritable && itemMap.ContainsKey(itemName)) {
+				return false;
+			}
+
+			if (itemMap.ContainsKey(itemName)) {
+				Remove(itemName);
+			}
+
+			itemMap.Add(itemName, itemObject);
+			return true;
+		}
+
+		public static bool Remove(string itemName) {
+			EnsureInited();
+
+			if (itemMap.ContainsKey(itemName)) {
+				itemMap.Remove(itemName);
+				return true;
+			}
+
+			return false;
+		}
+
+		public static GameObject Find(string itemName) {
+			EnsureInited();
+
+			if (itemMap.TryGetValue(itemName, out Item ret) == true) {
+				return ret.GetItemObject();
+			}
+
+			return null;
+		}
+
+		public static void Clear() {
+			EnsureInited();
+
+			itemMap.Clear();
+		}
+	}
+}
