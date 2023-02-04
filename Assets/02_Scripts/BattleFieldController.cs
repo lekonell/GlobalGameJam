9884ecb3681bff33ec53ff6db43cf115762f8d01
HighCl <fdsa1469@naver.com>
2023-02-04 10:29:25 +0900

    02041029

--- Assets/02_Scripts/BattleFieldController.cs
+++ Assets/02_Scripts/BattleFieldController.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class BattleFieldController : MonoBehaviour
 {
+    public List<GameObject> enemys = new List<GameObject>();
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Collider2D[] coll = Physics2D.OverlapBoxAll(transform.position, GetComponent<Collider2D>().bounds.size,0,1<<6);
+        foreach (var item in coll)
+        {
+            enemys.Add(item.gameObject);
+            item.gameObject.SetActive(false);
+        }
     }
 
     // Update is called once per frame
@@ -22,7 +29,11 @@ public class BattleFieldController : MonoBehaviour
         if(collision.gameObject.name == "TempPlayer" || collision.gameObject.name == "UnitRoot")
         {
             Managers.GM.isFight = true;
+            foreach (var item in enemys)
+            {
+                item.gameObject.SetActive(true);
+            }
             Destroy(gameObject);
-        }   
+        }
     }
 }
