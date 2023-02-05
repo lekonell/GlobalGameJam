703d6a36befa1c6eb4f4cf3167a0eac7d39e6db8
lekonell <secure@sogang.ac.kr>
2023-02-05 09:09:01 +0900

    asd

--- Assets/02_Scripts/UI/Scene/UI_InGameScene.cs
+++ Assets/02_Scripts/UI/Scene/UI_InGameScene.cs
@@ -139,13 +139,73 @@ public class UI_InGameScene : UI_Scene
         }
     }
 
-    //void OnClickTestGetGold(PointerEventData data = default)
-    //{
-    //    Managers.TempPlayer.gold++;
-    //    UpdateUI();
-    //}
+	public List<Sprite> goldSprites;
+	public int[] goldSpritesIndex = new int[2] { 0, 0 };
+	public bool isGoldSpritesInited = false;
 
-    public void UpdateUI_PlayerDie(PointerEventData data = default)
+	private GameObject digitWrapper = null;
+	private GameObject[] digits = new GameObject[2] { null, null };
+
+	public void PreloadGoldSprites() {
+		if (isGoldSpritesInited)
+			return;
+
+		/*
+		 * 00-07: 0->1 | 8
+		 * 07-14: 1->2 | 8
+		 * 14-21: 2->3 | 8
+		 * 21-28: 3->4 | 8
+		 * 28-35: 4->5 | 8
+		 * 36-42: 5->6 | 8
+		 * 42-48: 6->7 | 7
+		 * 48-55: 7->8 | 8
+		 * 55-62: 8->9 | 8
+		 * 62-69: 9->0 | 8
+		 */
+
+		digitWrapper = GameObject.Find("DigitWrapper");
+		digits[0] = digitWrapper.transform.Find("Digit_1").gameObject;
+		digits[1] = digitWrapper.transform.Find("Digit_2").gameObject;
+
+		goldSprites = new List<Sprite>();
+		int[] spriteCounts = new int[10] { 8, 8, 8, 8, 8, 8, 7, 7, 8, 8 };
+		goldSprites.AddRange(Resources.LoadAll<Sprite>("/GoldSprites/"));
+
+		isGoldSpritesInited = true;
+	}
+
+	public void UpdateGold(int goldFrom, int goldTo) {
+		PreloadGoldSprites();
+		// int playerGold = Camera.main.GetComponent<CameraManager>().player.GetComponent<PlayerControl>().GetPlayerGold();
+
+		string sGoldFrom = goldFrom.ToString();
+		string sGoldTo = goldTo.ToString();
+
+		for (int i = 0; i < 2; i++) {
+			if (sGoldFrom.Substring(i, 1) == sGoldTo.Substring(i, 1))
+				continue;
+
+			int diff = int.Parse(sGoldTo.Substring(i, 1)) - int.Parse(sGoldFrom.Substring(i, 1));
+
+			// StartCoroutine(UpdateGoldProcess(i, );
+		}
+	}
+
+	public IEnumerator UpdateGoldProcess(int idx, int digitFrom, int digitTo) {
+		int currentDigit = digitFrom;
+		while (currentDigit <= digitTo) {
+			// digits[0].GetComponent<Image>().sprite = goldSprites[];
+			yield return new WaitForSeconds(0.13f);
+		}
+	}
+
+	//void OnClickTestGetGold(PointerEventData data = default)
+	//{
+	//    Managers.TempPlayer.gold++;
+	//    UpdateUI();
+	//}
+
+	public void UpdateUI_PlayerDie(PointerEventData data = default)
     {
         int playerHP = (int)Camera.main.GetComponent<CameraManager>().player.GetComponent<PlayerControl>().GetPlayerHP();
 
